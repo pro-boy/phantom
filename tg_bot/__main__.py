a20ca556c914e2847f1e289fcdf7acea5f820e87
Zainul Abid <48980248+zainulhibath@users.noreply.github.com>
2020-05-12 00:43:18 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -28,12 +28,6 @@ from tg_bot.modules.helper_funcs.misc import paginate_modules
 PM_START_TEXT = """
 
 
-┈┈┈┈╱▔▔▔▔╲┈┈┈┈
-┈┈┈▕▕╲┊┊╱▏▏┈┈┈
-┈┈┈▕▕▂╱╲▂▏▏┈┈┈
-┈┈┈┈╲┊┊┊┊╱┈┈┈┈
-┈┈┈┈▕╲▂▂╱▏┈┈┈┈
-╱▔▔▔▔┊┊┊┊▔▔▔▔╲
 HOI    {}, MY NAME IS {} !
 
 I AM A GROUP MANAGER BOT MAINTAINED BY 
@@ -46,7 +40,9 @@ I AM COMPLETELY OPEN SOURCE
 
 MY SOURCE CODE IS AVAILABLE TO YOU
 
-[HERE](https://github.com/leobrownlee/phantom)
+[HERE](https://github.com/zainulhibath24/phantom)
+
+Join @ZainsProjects for more about Bots and programming
 
 """
 
