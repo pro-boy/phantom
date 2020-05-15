31aedb8063354840af97a435df6e4493b3038500
Zainul Abid <48980248+zainulhibath@users.noreply.github.com>
2020-05-16 00:01:59 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -30,16 +30,15 @@ PM_START_TEXT = """
 
 Hello    {}, My Name Is {} !
 
-I Am A Group Manager Bot Maintained By
-
-[This Person](tg://user?id={}).
+I Am A Group Manager Bot Maintained By [This Person](tg://user?id={}).
 
 I am a group management bot With Some Special Features.
-To add me to your group click ["HERE"](t.me/Aniebot?startgroup=botstart)
+To add me to your group click ["HERE"](t.me/Anierobot?startgroup=botstart)
 You can find my list of available commands with /help.
  
 See [Basic Configuration Checklist](t.me/Anienews/3) on how to secure your group.
 The support group chat is at @AnieSupport. Support Channel @AnieNews
+
 
 """
 
